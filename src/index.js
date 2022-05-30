4b9dde028b1a5207a02742e5c28e1088a6cc0400
Egor Kolev <kolevegor@gmail.com>
2022-05-30 12:40:40 +0300

    add hash-rout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container)
